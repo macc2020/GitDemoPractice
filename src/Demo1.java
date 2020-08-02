efbc0094b2d745d8c80019ade472c1091630d75e
macc2020 <makose00@gmail.com>
2020-08-01 20:27:01 -0500

    ne new coommit

--- src/Demo1.java
+++ src/Demo1.java
@@ -2,6 +2,8 @@ public class Demo1 {
     public static void main(String[] args) {
 
         System.out.println(" new commit");
+        System.out.println("ne new commit");
+
 
 //new commit 2
         //new commit 2
