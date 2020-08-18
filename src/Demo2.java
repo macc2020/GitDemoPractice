5492430e08f50e6c776c5e993df1ba83f956060a
macc2020 <makose00@gmail.com>
2020-08-19 01:43:40 +0300

    gitte gelme 2

--- src/Demo2.java
+++ src/Demo2.java
@@ -1,6 +1,6 @@
 public class Demo2 {
     public static void main(String[] args) {
-        System.out.println("degisim 2");
+        System.out.println("degisim 23456");
         //new marhaba
         // new merhaba 2
         //marhaba 3
